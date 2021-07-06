177958a7f103d681b0a2443f32f1da769bf1444e
gmzi <gastonmazieres@gmail.com>
2021-07-06 17:49:31 -0400

    /api/count 4

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.get('/api/count', async function (req, res) {
   //   const getCount = await axios.get(`${DATABASE_URL}/count`);
   const getCount = await axios.get(`https://db-shuffle.herokuapp.com/count`);
   //   return res.json(getCount.data);
-  res.end(getCount.data);
+  res.send(getCount.data);
 });
 
 module.exports = app;
