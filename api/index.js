2dbab9ad286da8ffe6d87ff6959e56cfc71c7fa5
gmzi <gastonmazieres@gmail.com>
2021-07-06 18:25:00 -0400

    /count-add

--- api/index.js
+++ api/index.js
@@ -1,4 +1,24 @@
-const app = require('express')();
+// require('dotenv').config();
+// const express = require('express');
+// const axios = require('axios');
+
+// const app = express();
+
+// const DATABASE_URL = process.env.DATABASE_URL;
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+DATABASE_URL = process.env.DATABASE_URL;
 
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
@@ -18,12 +38,18 @@ app.get('/api/hola', (req, res) => {
 });
 
 app.get('/api/count', async function (req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  //   const getCount = await axios.get(`${DATABASE_URL}/count`);
-  const getCount = await axios.get(`https://db-shuffle.herokuapp.com/count`);
+  const getCount = await axios.get(`${DATABASE_URL}/count`);
   //   return res.json(getCount.data);
-  res.send(getCount.data);
+  return res.json(getCount.data);
 });
+
+app.get('/count-add', async function (req, res) {
+  const call = await axios.get(`${DATABASE_URL}/count-add`);
+  return res.json(call.data);
+});
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
 
 module.exports = app;
