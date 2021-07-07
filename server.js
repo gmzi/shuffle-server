a6eb46b72288baf6ea666ec70de9149a28da59e3
gmzi <gastonmazieres@gmail.com>
2021-07-07 12:36:17 -0400

    correct server.js

--- server.js
+++ server.js
@@ -225,86 +225,81 @@ app.get('/track-last', async function (req, res) {
 });
 
 app.get('/recommendations', async function (req, res) {
-  try {
-    // GET REFERENCE TRACK FROM DB:
-    const seedTrackQuery = await axios.get(`${DATABASE_URL}/recommendations`);
+  // GET REFERENCE TRACK FROM DB:
+  const seedTrackQuery = await axios.get(`${DATABASE_URL}/recommendations`);
 
-    const fiveTrackUris = seedTrackQuery.data.slice(-5, seedTrackQuery.length);
+  const fiveTrackUris = seedTrackQuery.data.slice(-5, seedTrackQuery.length);
 
-    const fiveTrackIds = fiveTrackUris.map((r) =>
-      r.uri.replace('spotify:track:', '')
-    );
+  const fiveTrackIds = fiveTrackUris.map((r) =>
+    r.uri.replace('spotify:track:', '')
+  );
 
-    // GET AUTH TOKEN FROM SPOTIFY:
-    const credentials = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
-    const encodedCreds = Buffer.from(credentials).toString('base64');
-    const tokenReq = await axios.post(
-      'https://accounts.spotify.com/api/token',
-      'grant_type=client_credentials',
-      {
-        headers: { Authorization: `Basic ${encodedCreds}` },
-        responseType: 'json',
-      }
-    );
-    const token = tokenReq.data.access_token;
-
-    // GET ARTIST IDS FOR EACH TRACK FROM DB:
-    const allArtistIDS = [];
-    for (let id of fiveTrackIds) {
-      const trackDataReq = await axios.get(
-        `https://api.spotify.com/v1/tracks/${id}`,
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      const trackArtists = [];
-      trackDataReq.data.artists.map((a) => {
-        trackArtists.push(a.id);
-      });
-      allArtistIDS.push(trackArtists);
+  // GET AUTH TOKEN FROM SPOTIFY:
+  const credentials = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
+  const encodedCreds = Buffer.from(credentials).toString('base64');
+  const tokenReq = await axios.post(
+    'https://accounts.spotify.com/api/token',
+    'grant_type=client_credentials',
+    {
+      headers: { Authorization: `Basic ${encodedCreds}` },
+      responseType: 'json',
     }
+  );
+  const token = tokenReq.data.access_token;
 
-    const oneArtistIdByTrack = allArtistIDS.map((a) => {
-      return a[0];
-    });
-
-    const artistIdsSet = [...new Set(oneArtistIdByTrack)];
-    const artistIds = Array.from(artistIdsSet);
-    const idx = Math.floor(Math.random() * artistIds.length);
-
-    // GET RECOMMENDATIONS FROM SPOTIFY USING THE TRACKS FROM DB AS SEEDS:
-    const recommendationsReq = await axios.get(
-      `https://api.spotify.com/v1/recommendations`,
+  // GET ARTIST IDS FOR EACH TRACK FROM DB:
+  const allArtistIDS = [];
+  for (let id of fiveTrackIds) {
+    const trackDataReq = await axios.get(
+      `https://api.spotify.com/v1/tracks/${id}`,
       {
-        params: {
-          limit: 4,
-          seed_artists: artistIds[idx],
-          seed_tracks: fiveTrackIds,
-        },
         headers: { Authorization: `Bearer ${token}` },
-        responseType: 'json',
       }
     );
-
-    // TODO: REFACTOR THIS INTO A FUNCTION TO BE USED IN '/TRACKS' AND HERE
-    const readyTracks = {};
-
-    recommendationsReq.data.tracks.map((track, index) => {
-      readyTracks[index] = {
-        artists: track.artists.map((a) => a.name),
-        title: track.name,
-        uri: track.uri,
-        albumUrl:
-          track.album.images.length && track.album.images[1].url
-            ? track.album.images[1].url
-            : 'https://thumbs.dreamstime.com/b/spotify-logo-white-background-editorial-illustrative-printed-white-paper-logo-eps-vector-spotify-logo-white-background-206665979.jpg',
-      };
+    const trackArtists = [];
+    trackDataReq.data.artists.map((a) => {
+      trackArtists.push(a.id);
     });
-    return res.json(readyTracks);
-  } catch (e) {
-    console.log('problem fetching recommendations', e);
-    return;
+    allArtistIDS.push(trackArtists);
   }
+
+  const oneArtistIdByTrack = allArtistIDS.map((a) => {
+    return a[0];
+  });
+
+  const artistIdsSet = [...new Set(oneArtistIdByTrack)];
+  const artistIds = Array.from(artistIdsSet);
+  const idx = Math.floor(Math.random() * artistIds.length);
+
+  // GET RECOMMENDATIONS FROM SPOTIFY USING THE TRACKS FROM DB AS SEEDS:
+  const recommendationsReq = await axios.get(
+    `https://api.spotify.com/v1/recommendations`,
+    {
+      params: {
+        limit: 4,
+        seed_artists: artistIds[idx],
+        seed_tracks: fiveTrackIds,
+      },
+      headers: { Authorization: `Bearer ${token}` },
+      responseType: 'json',
+    }
+  );
+
+  // TODO: REFACTOR THIS INTO A FUNCTION TO BE USED IN '/TRACKS' AND HERE
+  const readyTracks = {};
+
+  recommendationsReq.data.tracks.map((track, index) => {
+    readyTracks[index] = {
+      artists: track.artists.map((a) => a.name),
+      title: track.name,
+      uri: track.uri,
+      albumUrl:
+        track.album.images.length && track.album.images[1].url
+          ? track.album.images[1].url
+          : 'https://thumbs.dreamstime.com/b/spotify-logo-white-background-editorial-illustrative-printed-white-paper-logo-eps-vector-spotify-logo-white-background-206665979.jpg',
+    };
+  });
+  return res.json(readyTracks);
 });
 
 app.post('/track-add', async function (req, res) {
