72beb1f8641eeae605cbfd28b92ebd321809db02
gmzi <gastonmazieres@gmail.com>
2021-07-07 09:06:04 -0400

    add spoti auth routes

--- api/index.js
+++ api/index.js
@@ -20,6 +20,202 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 DATABASE_URL = process.env.DATABASE_URL;
 
+// ---------------------------------------------------
+// SPOTIFY AUTH:
+app.post('/api/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  });
+
+  spotifyApi
+    .refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.accessToken,
+        expiresIn: data.body.expiresIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+let token;
+
+app.post('/api/login', (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      token = data.body.access_token;
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      res.sendStatus(400);
+    });
+});
+
+app.get('/api/token', async function (req, res) {
+  const credentials = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
+  const encodedCreds = Buffer.from(credentials).toString('base64');
+  try {
+    const spotiToken = await axios.post(
+      'https://accounts.spotify.com/api/token',
+      'grant_type=client_credentials',
+      {
+        headers: { Authorization: `Basic ${encodedCreds}` },
+        responseType: 'json',
+      }
+    );
+
+    return res.json(spotiToken.data);
+  } catch (e) {
+    console.log('token request failed', e);
+  }
+});
+
+// -----------------------------------------------------------
+// LOCAL ROUTES:
+
+app.get('/api/logout', (req, res) => {
+  token = '';
+  return res.json({ token });
+});
+
+app.get('/api/tracks', async function (req, res) {
+  const tracks = [];
+
+  const access_token = token;
+
+  try {
+    // GATHER ALL LIKED TRACKS
+
+    const spotifyApi = new SpotifyWebApi();
+    spotifyApi.setAccessToken(access_token);
+
+    const likedTracks = await getLikedTracks(spotifyApi);
+
+    // POUR LIKED TRACKS IN MAIN LIST:
+    for (let item of likedTracks) {
+      tracks.push(item.track);
+    }
+
+    // GATHER TRACKS FROM ALL PLAYLISTS:
+    const playlists = await getPlaylists(access_token);
+
+    const promises = {};
+
+    for (let id of playlists) {
+      promises[id] = axios.get(
+        `https://api.spotify.com/v1/playlists/${id}/tracks`,
+        {
+          headers: { Authorization: `Bearer ${access_token}` },
+          responseType: 'json',
+        }
+      );
+    }
+
+    const allTracksRaw = {};
+
+    for (let key in promises) {
+      allTracksRaw[key] = await promises[key];
+    }
+
+    const items = {};
+
+    for (let track in allTracksRaw) {
+      items[track] = allTracksRaw[track].data.items;
+    }
+
+    // POUR PLAYLISTS TRACKS IN MAIN LIST:
+    for (let key in items) {
+      for (let obj in items[key]) {
+        tracks.push(items[key][obj].track);
+      }
+    }
+
+    // PREPARE MAIN LIST TO BE SENT TO CLIENT:
+    const readyTracks = {};
+
+    tracks.map((track, index) => {
+      readyTracks[index] = {
+        artists: track.artists.map((a) => a.name),
+        title: track.name,
+        uri: track.uri,
+        albumUrl:
+          track.album.images.length && track.album.images[1].url
+            ? track.album.images[1].url
+            : 'https://thumbs.dreamstime.com/b/spotify-logo-white-background-editorial-illustrative-printed-white-paper-logo-eps-vector-spotify-logo-white-background-206665979.jpg',
+      };
+    });
+    return res.json(readyTracks);
+  } catch (e) {
+    console.log(
+      'server failed gathering tracks',
+      e.response.status,
+      e.response.headers
+    );
+  }
+});
+
+// ---------------------------------------------------------------------
+// Helper functions
+async function getPlaylists(access_token, offset = 0, items = []) {
+  return await axios
+    .get('https://api.spotify.com/v1/me/playlists', {
+      params: { limit: 50, offset: offset },
+      headers: { Authorization: `Bearer ${access_token}` },
+      responseType: 'json',
+    })
+    .then(async (res) => {
+      res.data.items.map((i) => items.push(i.id));
+      if (res.data.next) {
+        return await getPlaylists(access_token, (offset += 50), items);
+      } else {
+        return items;
+      }
+    });
+}
+
+async function getLikedTracks(
+  caller,
+  active = true,
+  offset = 0,
+  allTracks = []
+) {
+  if (active) {
+    return await caller
+      .getMySavedTracks({ limit: 20, offset: offset })
+      .then(async (res) => {
+        if (!res.body.next) {
+          active = false;
+          return allTracks;
+        }
+        res.body.items.map((t) => allTracks.push(t));
+        return await getLikedTracks(caller, active, (offset += 20), allTracks);
+      });
+  }
+  return Promise.all(calls);
+}
+
+// -------------------------------------------------------------
+// DATABASE ROUTES
+
 app.get('/api', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
@@ -52,8 +248,16 @@ app.get('/api/track-last', async function (req, res) {
   return res.json(call.data);
 });
 
-app.get('/api/hi', function (req, res) {
-  return res.send('hi how are you doing?');
+app.post('/api/track-add', async function (req, res) {
+  try {
+    const track = req.body.track;
+    const save = await axios.post(`${DATABASE_URL}/track-add`, {
+      track: track,
+    });
+    return res.json(save.data);
+  } catch (e) {
+    console.log('failed saving track in db', e);
+  }
 });
 
 app.get('/api/recommendations', async function (req, res) {
@@ -145,16 +349,8 @@ app.get('/api/recommendations', async function (req, res) {
   }
 });
 
-app.post('/api/track-add', async function (req, res) {
-  try {
-    const track = req.body.track;
-    const save = await axios.post(`${DATABASE_URL}/track-add`, {
-      track: track,
-    });
-    return res.json(save.data);
-  } catch (e) {
-    console.log('failed saving track in db', e);
-  }
+app.get('/api/hi', function (req, res) {
+  return res.send('hi how are you doing?');
 });
 
 const port = process.env.PORT || 3002;
